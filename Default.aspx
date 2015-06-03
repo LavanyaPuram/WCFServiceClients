d270bd763d47a7b41494a654e39927f086c8c6c3
LavanyaPuram <lavanya.puram90@gmail.com>
2015-06-03 19:09:51 +0100

    First Commit

---
+++ Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h2>Employee Information</h2>
+    <table style="border:2px solid black;font-family:Arial">
+        <tr>
+            <td width="52%"><b>Id</b></td>
+            <td width="55%"><asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td><b>Name</b></td>
+           <td><asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><b>Gender</b></td>
+            <td><asp:TextBox ID="txtGender" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><b>DOB</b></td>
+            <td><asp:TextBox ID="txtDOB" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td colspan="2"></td>
+        </tr>
+        <tr>
+            <td><asp:Button ID="btnGet" runat="server" Text="GetEmployee" Width="157" OnClick="btnGet_Click" /></td>
+            <td><asp:Button ID="btnSave" runat="server" Text ="Save Employee" Width="157" OnClick="btnSave_Click" /></td>
+        </tr>
+        <tr>
+            <td><asp:Button ID="btnUpdate" runat="server" Width="157" Text="Update Employee" OnClick="btnUpdate_Click" /></td>
+            <td><asp:Button ID="btnDelete" runat="server" Width="157" Text="Delete Employee" OnClick="btnDelete_Click" /></td>
+        </tr>
+        <tr>
+            <td colspan="2"><asp:Label ID="lblMessage" runat="server" ForeColor="Green" Font-Bold="true"></asp:Label></td>
+        </tr>
+    </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ServiceModel;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnGet_Click(object sender, EventArgs e)
+    {
+        EmployeeService.EmployeeServiceClient client = new EmployeeService.EmployeeServiceClient();
+        EmployeeService.Employee employee= client.GetEmployee(Convert.ToInt32(txtId.Text));
+        txtName.Text = employee.Name;
+        txtGender.Text = employee.Gender;
+        txtDOB.Text = employee.DOB.ToShortDateString();
+        lblMessage.Text = "Employee Retrieved";
+
+    }
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        EmployeeService.EmployeeServiceClient client = new EmployeeService.EmployeeServiceClient();
+        EmployeeService.Employee employee = new EmployeeService.Employee();
+        employee.Id = Convert.ToInt32(txtId.Text);
+        employee.Name = txtName.Text;
+        employee.Gender = txtGender.Text;
+        employee.DOB = Convert.ToDateTime(txtDOB.Text);
+        client.SaveEmployee(employee);
+        lblMessage.Text = "Employee Saved";
+    }
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        EmployeeService.EmployeeServiceClient client = new EmployeeService.EmployeeServiceClient();
+        EmployeeService.Employee employee = new EmployeeService.Employee();
+        employee.Id = Convert.ToInt32(txtId.Text);
+        employee.Name = txtName.Text;
+        employee.Gender = txtGender.Text;
+        employee.DOB = Convert.ToDateTime(txtDOB.Text);
+        client.UpdateEmployee(employee);
+        lblMessage.Text = "Employee Details Updated";
+    }
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        EmployeeService.EmployeeServiceClient client = new EmployeeService.EmployeeServiceClient();
+        client.DeleteEmployee(Convert.ToInt32(txtId.Text));
+        lblMessage.Text = "Employee Deleted";
+    }
+}
